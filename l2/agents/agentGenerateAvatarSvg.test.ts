9674c5fb13034053941f0962ed41ee0104875d82
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-03 16:22:17 -0300

    merge collab messages

---
+++ l2/agents/agentGenerateAvatarSvg.test.ts
@@ -0,0 +1,5 @@
+/// <mls shortName="agentGenerateAvatarSvg" project="102025" enhancement="_blank" folder="agents" />
+
+ import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
+ export const integrations: ICANIntegration[] = [];
+ export const tests: ICANTest[] = [];
