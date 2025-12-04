2f90de3be0707f896eb65dfa69804ff0f1b2d76f
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-04 11:38:16 -0300

    adjust collabChangeAvatar

--- l2/collabMessagesTests.test.ts
+++
@@ -1,5 +0,0 @@
-/// <mls shortName="collabMessagesTests" project="102025" enhancement="_blank" folder="" />
-
- import { ICANTest, ICANIntegration, ICANSchema  } from './_100554_tsTestAST';
- export const integrations: ICANIntegration[] = [];
- export const tests: ICANTest[] = [];
