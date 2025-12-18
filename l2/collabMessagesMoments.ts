7d2d2bd252de3ab787521ed5259f3a86e2b88091
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-18 11:06:14 -0300

    implements apps

---
+++ l2/collabMessagesMoments.ts
@@ -0,0 +1,15 @@
+/// <mls shortName="collabMessagesMoments" project="102025" enhancement="_100554_enhancementLit" />
+
+ import { html } from 'lit'; 
+ import { customElement, property } from 'lit/decorators.js';
+ import { StateLitElement } from '/_100554_/l2/stateLitElement.js';
+
+ @customElement('collab-messages-moments-102025')
+ export class CollabMessagesMoments102025 extends StateLitElement {
+    
+     @property() name: string = 'Somebody';
+
+     render() {
+         return html`<p> In develpoment </p>`;
+     }
+ }
