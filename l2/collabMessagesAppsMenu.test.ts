a8af3256519f19a4de4396505e65a2aae864e4f0
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2025-12-18 11:06:06 -0300

    implements apps

---
+++ l2/collabMessagesAppsMenu.test.ts
@@ -0,0 +1,5 @@
+/// <mls shortName="collabMessagesAppsMenu" project="102025" enhancement="_blank" folder="" />
+
+ import { ICANTest, ICANIntegration, ICANSchema  } from '/_100554_/l2/tsTestAST.js';
+ export const integrations: ICANIntegration[] = [];
+ export const tests: ICANTest[] = [];
