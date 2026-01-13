db2ebd4cbfeab1fd5f53ec48399b674f1a21c814
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-01-13 11:41:02 -0300

    refactor loadAgent

--- l2/collabMessagesTaskPreviewAgent.ts
+++ l2/collabMessagesTaskPreviewAgent.ts
@@ -3,7 +3,8 @@
 import { html, repeat, unsafeHTML } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { CollabLitElement } from '/_100554_/l2/collabLitElement.js';
-import { getTemporaryContext, getAgentInstanceByName } from '/_100554_/l2/aiAgentHelper.js';
+import { getTemporaryContext } from '/_100554_/l2/aiAgentHelper.js';
+import { loadAgent, executeBeforePrompt } from '/_100554_/l2/aiAgentOrchestration.js';
 
 @customElement('collab-messages-task-preview-agent-102025')
 export class CollabMessageTaskPreviewAgent extends CollabLitElement {
@@ -129,6 +130,7 @@ export class CollabMessageTaskPreviewAgent extends CollabLitElement {
                     <li>
                         <span>Execute</span>
                         <div style="float: right;">
+                            <span>(run again - test)</span>
                             <svg xmlns="http://www.w3.org/2000/svg" style="transform: rotateY(174deg); z-index:9999;width:15px; cursor:pointer" @click=${(e: MouseEvent) => { e.stopPropagation(); e.preventDefault(); this.replayForSupport(e.currentTarget as HTMLElement, (this.step as mls.msg.AIAgentStep).agentName, (this.step as mls.msg.AIAgentStep).interaction, (this.step as mls.msg.AIAgentStep).stepId) }} viewBox="0 0 576 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M209.4 39.5c-9.1-9.6-24.3-10-33.9-.9L33.8 173.2c-19.9 18.9-19.9 50.7 0 69.6L175.5 377.4c9.6 9.1 24.8 8.7 33.9-.9s8.7-24.8-.9-33.9L66.8 208 208.5 73.4c9.6-9.1 10-24.3 .9-33.9zM352 64c0-12.6-7.4-24.1-19-29.2s-25-3-34.4 5.4l-160 144c-6.7 6.1-10.6 14.7-10.6 23.8s3.9 17.7 10.6 23.8l160 144c9.4 8.5 22.9 10.6 34.4 5.4s19-16.6 19-29.2l0-64 32 0c53 0 96 43 96 96c0 30.4-12.8 47.9-22.2 56.7c-5.5 5.1-9.8 12-9.8 19.5c0 10.9 8.8 19.7 19.7 19.7c2.8 0 5.6-.6 8.1-1.9C494.5 467.9 576 417.3 576 304c0-97.2-78.8-176-176-176l-48 0 0-64z"/></svg>
                             <span class="result"></span>
                         </div>
@@ -236,7 +238,7 @@ export class CollabMessageTaskPreviewAgent extends CollabLitElement {
         if (!this.step || !this.step.agentName) return;
 
         try {
-            const agent = await getAgentInstanceByName(this.step.agentName);
+            const agent = await loadAgent(this.step.agentName);
             if (!agent) return;
             if (!agent || !agent.agentDescription) return;
             this.agentDescription = agent.agentDescription;
@@ -296,7 +298,7 @@ export class CollabMessageTaskPreviewAgent extends CollabLitElement {
 
 
             if (!this.task) throw new Error('Not found task');
-            const agent = await getAgentInstanceByName(agentName);
+            const agent = await loadAgent(agentName);
             if (!agent) throw new Error('Not found agent:' + agentName);
 
             const context = getTemporaryContext(this.task.PK, this.task.owner, 'context temporary');
