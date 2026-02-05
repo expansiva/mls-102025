1c101c6c5b726249efb236036b8b9e0a537d6aee
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-02-05 10:07:07 -0300

    save: l2/collabMessagesTaskPreviewAgent.ts

--- l2/collabMessagesTaskPreviewAgent.ts
+++ l2/collabMessagesTaskPreviewAgent.ts
@@ -1,4 +1,4 @@
-/// <mls shortName="collabMessagesTaskPreviewAgent" project="102025" enhancement="_100554_enhancementLit" />
+/// <mls fileReference="_102025_/l2/collabMessagesTaskPreviewAgent.ts" enhancement="_100554_enhancementLit" />
 
 import { html, repeat, unsafeHTML } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
