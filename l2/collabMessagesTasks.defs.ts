17797b175f4478a030282015bd9f2a952d38f481
Guilherme Pereira <39830378+GuilhermeExpansiva@users.noreply.github.com>
2026-02-05 10:07:26 -0300

    save: l2/collabMessagesTasks.defs.ts

--- l2/collabMessagesTasks.defs.ts
+++ l2/collabMessagesTasks.defs.ts
@@ -1,2 +1,2 @@
-/// <mls shortName="collabMessagesTasks" project="102025" enhancement="_blank" folder="" />
+/// <mls fileReference="_102025_/l2/collabMessagesTasks.defs.ts" enhancement="_blank" />
 
