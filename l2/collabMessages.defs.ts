4bc0b3ec4b2992b6e08ceecee0faa28ef264e6dc
Guilherme Santiago <57486730+santiagoExpansiva@users.noreply.github.com>
2026-05-04 10:51:47 -0300

    Update collabMessages.defs.ts

--- l2/collabMessages.defs.ts
+++ l2/collabMessages.defs.ts
@@ -1,4 +1,4 @@
-/// <mls fileReference="_102025_/l2/collabMessages.defs.ts" enhancement="_blank"/>  
+/// <mls fileReference="_102025_/l2/collabMessages.defs.ts" enhancement="_blank"/>   
 
 export const asis: mls.defs.AsIs =
 {
